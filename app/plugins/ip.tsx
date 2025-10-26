79dfdc03a31101e15ed9206f31fd8fafc3e34bd4
JLarky <jlarky@gmail.com>
2025-10-26 15:11:49 +0000

    refactor(ip): migrate IP SSE stream to JSX fragments via dataStarPatchElementsString for clean element patching

--- app/plugins/ip.tsx
+++ app/plugins/ip.tsx
@@ -1,6 +1,7 @@
-// app/plugins/ip.ts - Elysia IP routes plugin with persistence parity
+// app/plugins/ip.tsx - Elysia IP routes plugin with persistence parity (JSX fragments)
 import { Elysia } from "elysia";
 import { addIp, removeIp } from "../../domain/ip";
+import { dataStarPatchElementsString } from '../../rendering/datastar';
 import { rename } from "fs/promises";
 
 export const ipPlugin = (ipStore: string[]) => {
@@ -43,14 +44,26 @@ export const ipPlugin = (ipStore: string[]) => {
           let interval: any;
           function push() {
             try {
-              const status = `event: datastar-patch-elements\ndata: elements <div id=\"ips-status\" class=\"status\">Updated ${new Date().toLocaleTimeString()}<\\/div>\n\n`;
-              let listHtml = '<ul id="ips-ul">';
-              for (const ip of ipStore) listHtml += `<li>${ip}</li>`;
-              listHtml += "</ul>";
-              // Patch the wrapper div id="ips-list" (legacy markup) instead of duplicating ips-ul id
-              const ips = `event: datastar-patch-elements\ndata: elements <div id=\"ips-list\">${listHtml}<\\/div>\n\n`;
-              controller.enqueue(new TextEncoder().encode(status));
-              controller.enqueue(new TextEncoder().encode(ips));
+              const statusJsx = (
+                <div id="ips-status" class="status">
+                  Updated {new Date().toLocaleTimeString()}
+                </div>
+              );
+              const listJsx = (
+                <div id="ips-list">
+                  <ul id="ips-ul">
+                    {ipStore.map((ip) => (
+                      <li>{ip}</li>
+                    ))}
+                  </ul>
+                </div>
+              );
+              controller.enqueue(
+                new TextEncoder().encode(dataStarPatchElementsString(statusJsx)),
+              );
+              controller.enqueue(
+                new TextEncoder().encode(dataStarPatchElementsString(listJsx)),
+              );
             } catch (e) {
               clearInterval(interval);
               try {
